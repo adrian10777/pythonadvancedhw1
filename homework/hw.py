ab94a0573166986f75de01cc1babd68250649e19
Adrian Henriquez <adrian10777@hotmail.com>
2021-11-30 09:11:00 -0500

    firstcommit

---
+++ homework/hw.py
@@ -0,0 +1,8 @@
+# how do I import and write a function in this to help me solve the other exercise
+def findareasquare(length, width):
+    area= (length*width)
+    return(area)
+
+def findareacircle(radius):
+    area=(radius)
+    return(area)
